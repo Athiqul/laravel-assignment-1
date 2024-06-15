644d614215753561bde902236cf5221181e1191b
Athiqulhasan <athiqulhasan.4@gmail.com>
2024-06-15 17:41:11 +0600

    Avoiding Str_word_count function

--- problem-2/problem_2.php
+++ problem-2/problem_2.php
@@ -2,9 +2,40 @@
 //Get the files
 $data=file_get_contents('para.txt');
 
-//get number of paragraphs or new lines there
+function word_count(string $data){
+    $wordCount=0;
+    $charCount=0;
+    for($i=0;$i<strlen($data);$i++)
+    {
+        
+         if(($data[$i]>='A' && $data[$i]<="Z") || ($data[$i]>='a' && $data[$i]<="z")||($data[$i]>='0' && $data[$i]<='9'))
+         {
+          $charCount++;
+         }else{
+    
+            //If word is valid then word count increased
+            if($charCount>0)
+            {
+            
+                $wordCount++;
+                $charCount=0;
+            }
+    
+            //if spaces , others are not allowed
+    
+            $charCount=0;
+         } 
+         
+         
+    }
+    return $wordCount;
+}
 
-echo  str_word_count($data);
+echo word_count($data);
+
+
+
+
 //expected output 10
 
 ?>
